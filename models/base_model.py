3b622aaabe7d6e9079eb266c9292264139b5d961
jamal <jamalimmiss97@gmail.com>
2023-12-05 21:33:27 +0100

    creating basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,43 @@
 This is the BaseModel file
 """
 
+import uuid
+from datetime import datetime
+
 class BaseModel():
-    """
-    Represent the BaseModel class
-    """
-    pass
+    ''' Represent the class model
+    '''
+    def __init__(self, *args, **kwargs):
+        frmt = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key in kwargs:
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(kwargs[key], frmt)
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        ''' str representstion '''
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+    
+    def save(self):
+        ''' updates the public instance attribute 
+             updated_at with the current datetime
+        '''
+        self.updated_at = datetime.now()
+
+    
+    def to_dict(self):
+        '''
+        returns a dictionary containing all keys/values of __dict__
+        '''
+        mydict = self.__dict__.copy()
+        mydict["__class__"] = self.__class__.__name__
+        mydict["created_at"] =  mydict["created_at"].isoformat()
+        mydict["updated_at"] = mydict["updated_at"].isoformat()
+        return mydict
