25d1c106630f1434f9eb5373db8721adc90badfe
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-12-06 08:26:13 +0100

    add unittest for baseModel and create the console file

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is the entry file of our program
+"""
+
+import cmd
+
+class Console(cmd.Cmd):
+    """
+    This is the console class of our program (the interpreter)
+    """
+    prompt = "(hbnb) "
+
+    def do_EOF(self, _):
+        """
+        this will run if the interpreter is running on mode interactive
+        """
+        return True
+        
+    def do_quit(self, _):
+        """
+        this will quit the program
+        """
+        return True
+    
+if __name__ == "__main__":
+    Console().cmdloop()
