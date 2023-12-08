3b4b4ce6c6c8102eac5260e7b275bb9fd61f69b3
Med Amine Fh <37845480+medaminefh@users.noreply.github.com>
2023-12-08 16:29:21 +0100

    fixes (#7)

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ This is the entry file of our program
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """
     This is the console class of our program (the interpreter)
@@ -16,15 +17,19 @@ class HBNBCommand(cmd.Cmd):
         this will run if the interpreter is running on mode interactive
         """
         return True
-        
+
     def do_quit(self, _):
         """
         Quit command to exit the program
         """
         return True
-        
+
     def postloop(self):
+        """
+        post loop function
+        """
         print()
-    
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
