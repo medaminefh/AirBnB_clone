4c8fb8abe65414d5a813373dfcd18bc4d88c8921
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-12-09 20:17:31 +0100

    fix the unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,15 +16,8 @@ class TestBaseModel(unittest.TestCase):
         """
         Test instance creation with kwargs and without it
         """
-        my_model = BaseModel(name="Jamel")
         my_model2 = BaseModel()
         my_model2.name = "Mohamed Amine"
-        my_model_dict = my_model.to_dict()
-        print(my_model_dict)
-        self.assertEqual(my_model.name, "Jamel")
-        self.assertIs(hasattr(my_model, "id"), True)
-        self.assertIs(hasattr(my_model, "created_at"), True)
-        self.assertIs(hasattr(my_model, "updated_at"), True)
         self.assertEqual(my_model2.name, "Mohamed Amine")
         self.assertIs(hasattr(my_model2, "id"), True)
         self.assertIs(hasattr(my_model2, "created_at"), True)
