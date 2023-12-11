5cfef34fa371c625d6d15d05a591dfdfafbfc241
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-12-11 12:07:28 +0100

    fix object format

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Test FileStorage
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFilestorage(unittest.TestCase):
+    """
+    Testing my filestorage system
+    """
+
+    def test_creation(self):
+        pass
